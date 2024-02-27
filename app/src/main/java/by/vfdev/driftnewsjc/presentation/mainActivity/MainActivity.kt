1b8e9a52c9511e53ba91e90f55f1c8de6b384921
VasiliFralou <Dr1fteR20021998>
2024-02-27 23:18:38 +0300

    Add SystemBarsColors

--- app/src/main/java/by/vfdev/driftnewsjc/presentation/mainActivity/MainActivity.kt
+++ app/src/main/java/by/vfdev/driftnewsjc/presentation/mainActivity/MainActivity.kt
@@ -1,20 +1,35 @@
 package by.vfdev.driftnewsjc.presentation.mainActivity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
 import by.vfdev.driftnewsjc.domain.usecases.app_entry.AppEntryUseCases
 import androidx.hilt.navigation.compose.hiltViewModel
+import by.vfdev.driftnewsjc.presentation.navgraph.NavGraph
 import by.vfdev.driftnewsjc.presentation.onboarding.OnBoardingScreen
 import by.vfdev.driftnewsjc.presentation.onboarding.OnBoardingViewModel
 import by.vfdev.driftnewsjc.ui.theme.DriftNewsJCTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -31,9 +46,17 @@ class MainActivity : ComponentActivity() {
         }
         setContent {
             DriftNewsJCTheme(dynamicColor = false) {
-                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
-                    val viewModel: OnBoardingViewModel = hiltViewModel()
-                    OnBoardingScreen(onEvent = viewModel::onEvent)
+                val isSystemInDarkMode = isSystemInDarkTheme()
+                val systemUiColor = rememberSystemUiController()
+                SideEffect {
+                    systemUiColor.setSystemBarsColor(
+                        color = Color.Transparent,
+                        darkIcons = !isSystemInDarkMode
+                    )
+                }
+                //Add fillMaxSize()
+                Box(modifier = Modifier.background(MaterialTheme.colorScheme.background).fillMaxSize()) {
+                    NavGraph(startDestination = viewModel.startDestination.value)
                 }
             }
         }
